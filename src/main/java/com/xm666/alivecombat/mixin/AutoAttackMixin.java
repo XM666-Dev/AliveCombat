51192d96216c3a35ab2a50ca0dbec9a848e7847e
XM666-Dev <x94732568960@163.com>
2025-10-20 11:12:46 +0800

    Port to 1.21.10

--- src/main/java/com/xm666/alivecombat/mixin/AutoAttackMixin.java
+++ src/main/java/com/xm666/alivecombat/mixin/AutoAttackMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class AutoAttackMixin {
     @Mixin(Minecraft.class)
     private static class MinecraftMixin {
-        @ModifyExpressionValue(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;consumeClick()Z", ordinal = 13))
+        @ModifyExpressionValue(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;consumeClick()Z", ordinal = 14))
         private boolean modifyConsumeClick(boolean clicked) {
             if (clicked) {
                 AutoAttackHandler.timer.start();
