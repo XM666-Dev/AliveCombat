bedfbd44f29c25033092336392ca3d51f47b956f
XM666-Dev <x94732568960@163.com>
2025-10-21 13:15:17 +0800

    Fix config

--- src/main/java/com/xm666/alivecombat/handler/FakeSweepHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/FakeSweepHandler.java
@@ -15,8 +15,9 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
@@ -101,7 +102,6 @@ public class FakeSweepHandler {
         return (SimpleParticleType) BuiltInRegistries.PARTICLE_TYPE.get(ResourceLocation.tryParse(type));
     }
 
-    @EventBusSubscriber(modid = AliveCombat.MODID, value = Dist.CLIENT)
     public static class FakeSweepHandlerClient {
         @SubscribeEvent
         static void onAttackEntity(AttackEntityEvent event) {
@@ -116,8 +116,12 @@ public class FakeSweepHandler {
         }
     }
 
-    //@EventBusSubscriber(modid = AliveCombat.MODID, value = Dist.CLIENT)
+    @Mod(value = AliveCombat.MODID, dist = Dist.CLIENT)
     public static class FakeSweepHandlerConfig {
+        public FakeSweepHandlerConfig(IEventBus modEventBus) {
+            modEventBus.register(FakeSweepHandlerConfig.class);
+        }
+
         @SubscribeEvent
         static void onModConfigLoading(ModConfigEvent.Loading event) {
             toggle();
