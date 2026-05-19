d2d5ba9c3062dc26a27196f6d6416d067032e185
XM666-Dev <x94732568960@163.com>
2026-05-19 09:49:01 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
@@ -1,48 +1,26 @@
 package com.xm666.alivecombat.handler;
 
 import com.xm666.alivecombat.AliveCombat;
-import com.xm666.alivecombat.Config;
+import com.xm666.alivecombat.MixinConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
-import net.neoforged.neoforge.common.CommonHooks;
-import net.neoforged.neoforge.common.ItemAbilities;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 
 public class AttackParticleHandler {
     private static final RandomSource random = RandomSource.create();
-
-    @SuppressWarnings("DataFlowIssue")
-    private static boolean canSweep(Entity target) {
-        var mc = Minecraft.getInstance();
-        var player = mc.player;
-        var attackStrengthScale = player.getAttackStrengthScale(0.5F);
-        var full = attackStrengthScale > 0.9F;
-        var sprint = player.isSprinting() && full;
-
-        var crit = full && player.canCriticalAttack(target);
-        var critEvent = CommonHooks.fireCriticalHit(player, target, crit, crit ? 1.5F : 1.0F);
-
-        var sweep = player.isSweepAttack(full, critEvent.isCriticalHit() && critEvent.disableSweep(), sprint);
-        var sweepEvent = CommonHooks.fireSweepAttack(player, target, sweep);
-        return sweepEvent.isSweeping();
-    }
 
     @SuppressWarnings("DataFlowIssue")
     private static void sweepAttack() {
