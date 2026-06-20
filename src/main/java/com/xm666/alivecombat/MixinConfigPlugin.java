f0bce0d2100f94e1b24a0b9a49ff56d9215d2559
XM666-Dev <x94732568960@163.com>
2026-06-20 12:20:07 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/MixinConfigPlugin.java
+++ src/main/java/com/xm666/alivecombat/MixinConfigPlugin.java
@@ -5,6 +5,7 @@ import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.mclanguageprovider.MinecraftModContainer;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -17,7 +18,7 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
 
     @Override
     public void onLoad(String mixinPackage) {
-        var container = new MinecraftModContainer(FMLLoader.getLoadingModList().getModFileById(AliveCombat.MODID).getMods().getFirst());
+        var container = new MinecraftModContainer(FMLLoader.getCurrent().getLoadingModList().getModFileById(AliveCombat.MODID).getMods().getFirst());
         container.registerConfig(ModConfig.Type.STARTUP, MixinConfig.SPEC);
         this.mixinPackage = mixinPackage;
     }
@@ -32,7 +33,7 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
         var packageFromIndex = mixinPackage.length() + 1;
         var packageToIndex = mixinClassName.indexOf('.', packageFromIndex);
         var packageName = mixinClassName.substring(packageFromIndex, packageToIndex);
-        if (FMLLoader.getLoadingModList().getModFileById(packageName) == null) return false;
+        if (FMLLoader.getCurrent().getLoadingModList().getModFileById(packageName) == null) return false;
 
         var toIndex = mixinClassName.lastIndexOf('$');
         var fromIndex = Math.max(
@@ -40,7 +41,7 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
                 mixinClassName.lastIndexOf('$', toIndex - 1)
         ) + 1;
         var path = mixinClassName.substring(fromIndex, toIndex);
-        path = StringUtils.removeEnd(path, "Mixin");
+        path = Strings.CS.removeEnd(path, "Mixin");
         path = StringUtils.uncapitalize(path);
         path += "Enabled";
         var value = MixinConfig.SPEC.getValues().<ModConfigSpec.BooleanValue>get(path);
