7a0b74cad89a066f7cab60e1105659a31f7fca20
XM666-Dev <x94732568960@163.com>
2026-06-20 21:50:57 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/handler/HeldAttackHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/HeldAttackHandler.java
@@ -1,8 +1,8 @@
 package com.xm666.alivecombat.handler;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.common.ItemAbilities;
 
 public class HeldAttackHandler {
     public static boolean heldAttack = false;
@@ -11,6 +11,6 @@ public class HeldAttackHandler {
         if (!(entity instanceof LivingEntity living) || !living.isUsingItem()) return false;
 
         var stack = living.getUseItem();
-        return !stack.isEmpty() && stack.canPerformAction(ItemAbilities.SHIELD_BLOCK);
+        return stack.has(DataComponents.BLOCKS_ATTACKS);
     }
 }
