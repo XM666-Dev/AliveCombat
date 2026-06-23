94cc2d6bcf305d2de36a1d0d9904da490195505d
XM666-Dev <x94732568960@163.com>
2026-06-23 19:35:49 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/Config.java
+++ src/main/java/com/xm666/alivecombat/Config.java
@@ -5,8 +5,6 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 @Mod(value = AliveCombat.MODID, dist = Dist.CLIENT)
@@ -35,6 +33,5 @@ public class Config {
 
     public Config(ModContainer container) {
         container.registerConfig(ModConfig.Type.CLIENT, SPEC);
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 }
