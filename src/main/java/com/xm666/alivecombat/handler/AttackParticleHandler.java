fa7733d162c353b58de9fb956a6f6a37c81bbe0b
XM666-Dev <x94732568960@163.com>
2026-06-24 20:24:07 +0800

    Add AttackParticle better rotation

--- src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
@@ -10,6 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -17,17 +18,54 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.common.Tags;
-import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
+import net.neoforged.neoforge.event.entity.player.CriticalHitEvent;
 
 public class AttackParticleHandler {
     private static final RandomSource random = RandomSource.create();
 
-    private static void sweepAttack() {
-        var player = Minecraft.getInstance().player;
+    private static boolean canAddParticle(Player player) {
+        if (!player.isLocalPlayer()) return false;
+
+        var weapon = player.getWeaponItem();
+        if (!weapon.is(Tags.Items.MELEE_WEAPON_TOOLS)) return false;
+
+        var attackStrengthScale = player.getAttackStrengthScale(0.5F);
+        return attackStrengthScale > 0.9F;
+    }
+
+    private static void addParticle(Player player, boolean isCriticalHit, boolean isSprintHit) {
         var xOffset = -Mth.sin(player.getYRot() * Mth.DEG_TO_RAD);
         var zOffset = Mth.cos(player.getYRot() * Mth.DEG_TO_RAD);
-        player.level().playSound(player, player.getX(), player.getY(), player.getZ(), getSoundEvent(), player.getSoundSource(), 1.0F, 1.0F);
-        sendParticles(getParticleType(), player.getX() + xOffset, player.getY(0.5), player.getZ() + zOffset, 0, xOffset, 0.0, zOffset, 0.0);
+        var x = player.getX();
+        var y = player.getY();
+        var z = player.getZ();
+        var soundEvent = getSoundEvent();
+        var soundSource = player.getSoundSource();
+        var partialType = getParticleType();
+        var center = player.getY(0.5);
+        var roll = getParticleRoll(isCriticalHit, isSprintHit);
+        player.level().playSound(player, x, y, z, soundEvent, soundSource, 1.0F, 1.0F);
+        sendParticles(partialType, x + xOffset, center, z + zOffset, 0, 0.0, roll, zOffset, 1.0);
+    }
+
+    private static SoundEvent getSoundEvent() {
+        var location = "entity.player.attack.sweep";
+        return BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.tryParse(location));
+    }
+
+    private static SimpleParticleType getParticleType() {
+        var location = "minecraft:sweep_attack";
+        return (SimpleParticleType) BuiltInRegistries.PARTICLE_TYPE.get(ResourceLocation.tryParse(location));
+    }
+
+    private static double getParticleRoll(boolean isCriticalHit, boolean isSprintHit) {
+        if (isCriticalHit) {
+            return Mth.PI * 0.5;
+        } else if (isSprintHit) {
+            return 0.0;
+        } else {
+            return Mth.PI * 0.25;
+        }
     }
 
     private static <T extends ParticleOptions> void sendParticles(T type, double posX, double posY, double posZ, int particleCount, double xOffset, double yOffset, double zOffset, double speed) {
@@ -55,30 +93,13 @@ public class AttackParticleHandler {
         }
     }
 
-    private static SoundEvent getSoundEvent() {
-        var type = "entity.player.attack.sweep";
-        return BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.tryParse(type));
-    }
-
-    private static SimpleParticleType getParticleType() {
-        var type = "minecraft:sweep_attack";
-        return (SimpleParticleType) BuiltInRegistries.PARTICLE_TYPE.get(ResourceLocation.tryParse(type));
-    }
-
     public static class AttackParticleClient {
         @SubscribeEvent
-        public static void onAttackEntity(AttackEntityEvent event) {
+        public static void onCriticalHit(CriticalHitEvent event) {
             var player = event.getEntity();
-            if (!player.isLocalPlayer()) return;
+            if (!canAddParticle(player)) return;
 
-            var weapon = player.getWeaponItem();
-            if (!weapon.is(Tags.Items.MELEE_WEAPON_TOOLS)) return;
-
-            var attackStrengthScale = player.getAttackStrengthScale(0.5F);
-            var full = attackStrengthScale > 0.9F;
-            if (!full) return;
-
-            sweepAttack();
+            addParticle(player, event.isVanillaCritical(), event.getEntity().isSprinting());
         }
     }
 
--- src/main/java/com/xm666/alivecombat/mixin/alivecombat/AttackParticleMixin.java
+++ src/main/java/com/xm666/alivecombat/mixin/alivecombat/AttackParticleMixin.java
@@ -1,16 +1,31 @@
 package com.xm666.alivecombat.mixin.alivecombat;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.particle.AttackSweepParticle;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @OnlyIn(Dist.CLIENT)
 public class AttackParticleMixin {
+    @Mixin(AttackSweepParticle.Provider.class)
+    private static class Provider {
+        @ModifyReturnValue(method = "createParticle(Lnet/minecraft/core/particles/SimpleParticleType;Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("RETURN"))
+        private Particle modifyParticle(Particle original, SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed) {
+            original.roll = original.oRoll = (float) ySpeed;
+            return original;
+        }
+    }
+
     @Mixin(ClientPacketListener.class)
     private static class ClientPacketListenerMixin {
         @WrapMethod(method = "handleParticleEvent")
