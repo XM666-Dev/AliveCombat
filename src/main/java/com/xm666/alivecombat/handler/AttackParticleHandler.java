a7da22d76f28280de98b276c948e98fb0c603aef
XM666-Dev <x94732568960@163.com>
2026-08-06 12:44:02 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
@@ -19,6 +19,7 @@ import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
