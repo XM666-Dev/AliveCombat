7b7fd16aa4581364ecf114201399f37e4608e539
XM666-Dev <x94732568960@163.com>
2026-08-06 12:42:08 +0800

    Fix

--- src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
+++ src/main/java/com/xm666/alivecombat/handler/AttackParticleHandler.java
@@ -19,7 +19,6 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.NeoForge;
